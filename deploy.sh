2a72cd30fca4112087ecbcbd5c827bfdf4b0049b
Sallieu-99 <sallieusesay441@gmail.com>
2024-12-01 19:07:16 +0000

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 Application is ready to be deployed in the development environment.
 development must be reviewed before deployment
 deployment must be reviewed by managers
-Sallieu is a billioner
+Sallieu is a billioner.
